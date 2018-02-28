3fc36561b2f350d81eb73a28a4d7a274d7949be7
Ruben <ruben.yedigaryan@gmail.com>
2018-02-28 16:22:12 +0400

    Finally CMake configured and the project runs...

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include "ArcanoidGameManager.h"
+
 int main() {
+    ArcanoidGameManager& manage = ArcanoidGameManager::getManager();
+    ArcanoidGameDrawer drawer(Size(10, 10));
+    ArcanoidGameEngine& engine = ArcanoidGameEngine::getEngine();
+    manage.setDrawer(&drawer);
+    manage.setEngine(&engine);
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
